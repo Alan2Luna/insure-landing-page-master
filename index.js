3cfbe3e3a192f00fdcd2abe1b5db965f87db550d
Alan Luna <aklt1994@gmail.com>
2020-04-28 22:03:51 +0200

    mobile version finished

---
+++ index.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector('.header__navbtn');
+let navbar = document.querySelector('.header__nav');
+
+btn.addEventListener('click', ()  => {
+    btn.classList.toggle('open');
+})
